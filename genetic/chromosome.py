6136f5def19c990789dcc09b9239858ed203bf09
Philip Tempelman <tempelmanphilip@gmail.com>
2023-10-04 15:16:50 -0400

    add base implementation for mutate and crossover

--- genetic/chromosome.py
+++ genetic/chromosome.py
@@ -10,40 +10,38 @@ from preprocessing.all import get_all_preprocessing
 
 class Chromosome:
     def __init__(self) -> None:
-        self.preprocessing = None
-        self.model = None
-    
-
-    def random(self):
         self.preprocessing = random.choice(get_all_preprocessing())
         self.model = random.choice(get_all_models())
         self.model_params = get_random_model_params(self.model)
 
-
-    def mutate(pipeline: Chromosome):
+def mutate(self, pipeline: Chromosome):
+    
+    def add_preprocessing_step(pipeline: Chromosome):
+        pipeline.preprocessing.insert(random.randint(0, len(self.preprocessing)), random.choice(get_all_preprocessing()))
+    
+    def remove_preprocessing_step(pipeline: Chromosome):
+        pipeline.preprocessing.remove(random.randint(0, len(self.preprocessing)))
+    
+    def switch_model(pipeline: Chromosome):
+        pipeline.model = random.choice(get_all_models())
         
-        def add_preprocessing_step(pipeline: Chromosome):
-            pass
+    def mutate_model_params(pipeline: Chromosome):
+        pass
+
+
+def crossover(pipeline_a: Chromosome, pipeline_b: Chromosome):
+
+    def exchange_preprocessing(pipeline_a: Chromosome, pipeline_b: Chromosome):
+        preprocessing_a = pipeline_a.preprocessing
+        pipeline_a.preprocessing = pipeline_b.preprocessing
+        pipeline_b.preprocessing = preprocessing_a
         
-        def remove_preprocessing_step(pipeline: Chromosome):
-            pass
-        
-        def switch_model(pipeline: Chromosome):
-            pass
-            
-        def mutate_model_params(pipeline: Chromosome):
-            pass
+    def exchange_model(pipeline_a: Chromosome, pipeline_b: Chromosome):
+        model_a = pipeline_a.model
+        pipeline_a.model = pipeline_b.model
+        pipeline_b.model = model_a
 
-
-    def crossover(pipeline_a: Chromosome, pipeline_b: Chromosome):
-
-        def exchange_preprocessing(pipeline_a: Chromosome, pipeline_b: Chromosome):
-            pass
-            
-        def exchange_model(pipeline_a: Chromosome, pipeline_b: Chromosome):
-            pass
-
-        def exchange_model_parameters(pipeline_a: Chromosome, pipeline_b: Chromosome):
-            pass
+    def exchange_model_parameters(pipeline_a: Chromosome, pipeline_b: Chromosome):
+        pass
 
 
